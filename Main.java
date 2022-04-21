0c4945e23d86ba7ea3312b1f6f31a2dc2c358d5e
ts092000 <ts092000@gmail.com>
2022-04-21 13:25:02 +0700

    Lambda Expressions: Chapter 3 => Writing Suppliers, Consumers, Predicates, and Functions

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 package com.bkitsolution;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class Main {
@@ -25,20 +29,53 @@ public class Main {
 //        d2.draw();
 
         //Supplier
-        Supplier<String> supplier = () -> {
-            System.out.println("I am inside the Supplier");
-            return "Hello";
-        };
-        String string = supplier.get();
-        System.out.println("string = " + string);
+//        Supplier<String> supplier = () -> {
+//            System.out.println("I am inside the Supplier");
+//            return "Hello";
+//        };
+//        String string = supplier.get();
+//        System.out.println("string = " + string);
 
         //Consumer
 //        Consumer<String> consumer = System.out::println;
-        Consumer<String> consumer = (String s) -> {
-            System.out.println("I am inside the Consumer");
-            System.out.println(s);
-        };
-        consumer.accept("Hello");
+//        Consumer<String> consumer = (String s) -> {
+//            System.out.println("I am inside the Consumer");
+//            System.out.println(s);
+//        };
+//        consumer.accept("Hello");
+
+//        List<String> strings =
+//                new ArrayList<>(List.of("one", "two", "three", "four", "five"));
+////        Predicate<String> filter = string -> string.startsWith("t");
+////        Predicate<String> filter2 = string -> !string.startsWith("t");
+////        strings.removeIf(filter);
+//        strings.removeIf(string -> string.startsWith("t"));
+////        strings.removeIf(string -> !string.startsWith("t"));
+////        Consumer<String> action = System.out::println;
+////        Consumer<String> action = string -> System.out.println(string);
+////        strings.forEach(System.out::println);
+//        strings.forEach(string -> System.out.println(string));
+
+        User sarah = new User("Sarah");
+        User james = new User("James");
+        User mary = new User("Mary");
+        User john = new User("John");
+
+        List<User> users = new ArrayList<>(List.of(sarah, james, mary, john));
+        List<String> names = new ArrayList<>();
+        Function<User, String> toName = (User user) -> user.getName();
+        for (User user: users) {
+//            System.out.println("Name: " + toName.apply(user));
+            String name = toName.apply(user);
+            names.add(name);
+        }
+//        Consumer<User> action = System.out::println;
+//        Consumer<User> action = u -> System.out.println(u);
+//        users.forEach(System.out::println);
+        users.forEach(u -> System.out.println(u));
+//        names.forEach(System.out::println);
+//        names.forEach((String s) -> System.out.println(s));
+        names.forEach(s -> System.out.println(s));
     }
 
 }
---
+++ User.java
@@ -0,0 +1,20 @@
+package com.bkitsolution;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+}
